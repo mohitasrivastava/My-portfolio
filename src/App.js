2e06daf7b6b33b59beafa23998b0b72e8c1a1db7
unknown <srivastavamohita18@gmail.com>
2022-06-26 06:26:08 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -111,7 +111,7 @@ function App() {
 
         <div className="relative" id="home">
           <Spline scene="https://prod.spline.design/oo6IxFu8UDjFUmjD/scene.spline" />
-          <div className="absolute bottom-10 w-full flex justify-center items-center">
+          <div className="relative bottom-10 w-full flex justify-center items-center">
             <div className="shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl ">
               <p className="text-white">Press and drag to orbit</p>
             </div>
